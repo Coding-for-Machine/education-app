7ed99ba540d6d09207febc5d63595bcf9a351bc5
CodingforMachine <codingformachine@gmail.com>
2025-05-07 04:15:34 +0500

    my commits

--- app/next-intl.config.ts
+++
@@ -1,9 +0,0 @@
-// next-intl.config.ts
-import { NextIntlConfig } from 'next-intl'
-
-const config: NextIntlConfig = {
-  locales: ['en', 'uz', 'ru'],
-  defaultLocale: 'en'
-};
-
-export default config;
--- app/next.config.js
+++
@@ -1,6 +0,0 @@
-// next.config.js
-const withNextIntl = require('next-intl/plugin')('./next-intl.config.ts');
-
-module.exports = withNextIntl({
-  // Boshqa Next.js sozlamalaringiz shu yerda
-});
